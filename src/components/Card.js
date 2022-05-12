143293a6b48db77686626cab9c7f35f83d173b6d
Artan Myrtolli <artanmyrtolli@gmail.com>
2022-05-12 18:10:24 -0400

    Add additional info on movie cards hover

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../styles/Card.css';
 
-const Card = (props) => {
-  console.log(props);
-  return (
-    <img className='card__poster' src={props.img} />
-  )
+class Card extends Component {
+  constructor(){
+    super();
+    this.state={
+      show: false
+    }
+  }
+
+  handleHover = () => {
+    this.setState({
+      show: !this.state.show
+    })
+  }
+
+  render = () => {
+    return (
+        <div className='card__poster' 
+        onMouseEnter={this.handleHover} 
+        onMouseLeave={this.handleHover} 
+        src={this.props.img} 
+        style={{backgroundImage: `url(${this.props.img})`}}>
+          <h2 className={this.state.show ? 'card__hover-info' : 'hidden'}>{this.props.title}</h2>
+          <h2 className={this.state.show ? 'card__hover-info' : 'hidden'}>{this.props.year}</h2>
+          <h2 className={this.state.show ? 'card__hover-info' : 'hidden'}>{this.props.rating}</h2>
+        </div>
+      )
+    }
+  
 };
 
 export default Card;
