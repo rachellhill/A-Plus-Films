d6145fdd3847b43801344d54deea3f7013c5a1ba
Rachel Hill <hillrachel187@gmail.com>
2022-05-16 20:48:41 -0500

    Add styling for movie display

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -4,10 +4,11 @@ import '../styles/Backdrop.css'
 
 const Backdrop = (props) => {
     return (
+      <div className='movie__backdrop-box'>
         <div className="movie__backdrop" style={{backgroundImage: `url(${props.movie.backdrop_path})`}}>
-            <GeneralInfo title={props.movie.title} rating={props.movie.average_rating} genres={props.movie.genres}/>
         </div>
-
+        <GeneralInfo title={props.movie.title} rating={props.movie.average_rating} genres={props.movie.genres}/>
+      </div>
     )
 }
 
--- src/components/DetailedInfo.js
+++ src/components/DetailedInfo.js
@@ -8,9 +8,20 @@ import Specs from './Specs'
 const DetailedInfo = (props) => {
     return (
         <div className='movie__detailed-info'>
-            <Specs specs={props.movie}/>
-            <Synopsis overview={props.movie.overview}/>
-            <Trailers trailers={props.trailers} />
+            <div className='movie__specs'>
+              <Specs specs={props.movie}/>
+            </div>
+            <div className='movie__synopsis'>
+              <div className="movie__synopsis-header">
+                <h5>Synopsis:</h5>
+              </div>
+              {!props.movie.overview && <h6>No Synopsis Available</h6>}
+              <Synopsis overview={props.movie.overview}/>
+            </div>
+            <div className="movie__trailers">
+              {!props.trailers.length && <h6>No Trailers Available</h6>}
+              <Trailers trailers={props.trailers} />
+            </div>
         </div>
     )
 }
