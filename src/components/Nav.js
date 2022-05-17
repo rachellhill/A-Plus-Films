056b7f571cfc5ce38040d7caa4964f041eee5f08
Rachel Hill <hillrachel187@gmail.com>
2022-05-16 21:02:17 -0500

    Add styling to home button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,6 @@ const Nav = () => {
       <h1 className='nav__header'>A+ Films</h1>
       <div className='nav__buttons-box'>
         <button className='nav__home-button'>Home</button>
-        <button className='nav__login-button'>Login</button>
       </div>
     </nav>
   );
